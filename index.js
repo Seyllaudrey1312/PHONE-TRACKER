60716559290d60feb76c33c529b6ec288c29458a
Seyllaudrey1312 <audreyseyll3@gmail.com>
2026-01-06 13:21:54 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-'use strict';
+"use strict"
 
-/** @type {import('.')} */
-var $gOPD = require('./gOPD');
+var mergeModules = require("../lib/helpers/merge-exports")
 
-if ($gOPD) {
-	try {
-		$gOPD([], 'length');
-	} catch (e) {
-		// IE 8 has a broken gOPD
-		$gOPD = null;
-	}
+// Update this array if you add/rename/remove files in this directory.
+// We support Browserify by skipping automatic module discovery and requiring modules directly.
+var modules = [
+  require("./internal"),
+  require("./utf32"),
+  require("./utf16"),
+  require("./utf7"),
+  require("./sbcs-codec"),
+  require("./sbcs-data"),
+  require("./sbcs-data-generated"),
+  require("./dbcs-codec"),
+  require("./dbcs-data")
+]
+
+// Put all encoding/alias/codec definitions to single object and export it.
+for (var i = 0; i < modules.length; i++) {
+  var module = modules[i]
+  mergeModules(exports, module)
 }
-
-module.exports = $gOPD;
